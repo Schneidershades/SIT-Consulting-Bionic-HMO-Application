5a7a83448ea2a40d833479843dd079c1a2cd6d8e
schneidershades <schneidershades@gmail.com>
2019-06-27 17:45:35 +0100

    pre auth

--- app/Http/Controllers/HMO/PreAuthorizationController.php
+++ app/Http/Controllers/HMO/PreAuthorizationController.php
@@ -10,18 +10,44 @@ class PreAuthorizationController extends Controller
 {
     public function index()
     {
-    	$transactions = TariffDrugTransaction::where('pre_authorization', true)->where('status', 'pending')->get();
+    	$transactions = TariffDrugTransaction::where('pre_authorization', true)->where('status', 'pending')->where('hmo_id', auth()->user()->userable->id)->get();
     	return view('dashboard.hmo.pre-authorization.index')
                 ->with('transactions', $transactions);
     }
 
+    public function allPreAuthRequest()
+    {
+    	$transactions = TariffDrugTransaction::where('pre_authorization', true)where('hmo_id', auth()->user()->userable->id)->get();
+    	return view('dashboard.hmo.pre-authorization.index')
+                ->with('transactions', $transactions);
+    }
+
+    public function showPreAuthDetails()
+    {
+    	$transaction = TariffDrugTransaction::where('identifier', $identifier)->first();
+    	return view('dashboard.hmo.pre-authorization.show')
+                ->with('transaction', $transaction);
+    }
+
     public function verify($identifier)
     {
-
+    	$transactions = TariffDrugTransaction::where('identifier', $identifier)->first();
+    	if($transaction->status == 'verified'){
+    		return redirect()->back();
+    	}
+    	$transaction->status = 'verified';
+    	$transaction->save();
+    	return redirect()->back();
     }
 
     public function cancel($identifier)
     {
-
+    	$transactions = TariffDrugTransaction::where('identifier', $identifier)->first();
+    	if($transaction->status == 'cancelled'){
+    		return redirect()->back();
+    	}
+    	$transaction->status = 'cancelled';
+    	$transaction->save();
+    	return redirect()->back();
     }
 }
