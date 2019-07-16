e664f0ef4999d8f300ad5bfd6534460968cdefaf
schneidershades <schneidershades@gmail.com>
2019-07-16 23:47:50 +0100

    seeder

--- database/seeds/EnrolleesTableSeeder.php
+++ database/seeds/EnrolleesTableSeeder.php
@@ -1,11 +1,37 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Enrollee;
 
 class EnrolleesTableSeeder extends Seeder
 {
     public function run()
     {
-        //
+    	Enrollee::create([
+    		'hmo_id'=>'1',
+		    'hcp_id'=>'3000',
+		    'referral_code'=> 'nie',
+		    'health_plan_id'=> 5,
+		    'first_name' => 'schneider',
+		    'middle_name' = 'busayo',
+		    'last_name' => 'komolafe',
+		    'gender' => 'male',
+		    'next_of_kin' => 'baxi',
+		    'next_of_kin_relationship' => 'baxi',
+		    'next_of_kin_address' => 'baxi',
+		    'email' => 'buskome@yahoo.com',
+		    'residential_address' => 'Abuja',
+		    'residential_state' => 'Abuja',
+		    'phone_number' => '3292892333',
+		    'telephone_number' => '98932893233',
+		    'genotype' => 'A',
+		    'blood_group' => 'O+',
+		    'occupation' => 'software',
+		    'company' => 'sit consulting',
+		    'company_sector' => 'software',
+		    'nationality' => 'nigerian',
+		    'parent_id' => null;
+		    'operator_user_id' => 1;
+    	]);
     }
 }
