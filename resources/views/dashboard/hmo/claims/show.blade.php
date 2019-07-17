5f4b4e91c87582f616e0817f961d470d48f1db65
schneidershades <schneidershades@gmail.com>
2019-07-17 12:10:36 +0100

    change to claims

--- resources/views/dashboard/hmo/claims/show.blade.php
+++ resources/views/dashboard/hmo/claims/show.blade.php
@@ -13,16 +13,16 @@
 <!-- Breadcrumb-->
 <div class="row pt-2 pb-2">
 	<div class="col-sm-9">
-		<h4 class="page-title">Bill for {{$bill->enrollee->first_name}} {{$bill->enrollee->middle_name}} {{$bill->enrollee->last_name}} in {{$bill->hcp->hcp_name}}</h4>
+		<h4 class="page-title">Claim Service for {{$claim->hcp->hcp_name}}</h4>
 		<ol class="breadcrumb">
 			<li class="breadcrumb-item"><a href="javaScript:void();">{{auth()->user()->userable->hmo_name}}</a></li>
 			<li class="breadcrumb-item"><a href="javaScript:void();">Dashboard</a></li>
-			<li class="breadcrumb-item active" aria-current="page"> Bill for {{$bill->enrollee->first_name}} {{$bill->enrollee->middle_name}} {{$bill->enrollee->last_name}} in {{$bill->hcp->hcp_name}}</li>
+			<li class="breadcrumb-item active" aria-current="page"> Claim for {{$claim->hcp->hcp_name}}</li>
 		</ol>
 	</div>
 	<div class="col-sm-3">
 		<div class="btn-group float-sm-right">
-			<a href="{{route('bills.index')}}"><button type="button" class="btn btn-outline-primary waves-effect waves-light"><i class="fa fa-left mr-1"></i> Back</button></a>
+			<a href="{{route('claims.index')}}"><button type="button" class="btn btn-outline-primary waves-effect waves-light"><i class="fa fa-left mr-1"></i> Back</button></a>
 		</div>
 	</div>
 </div>
@@ -33,26 +33,9 @@
 	<div class="col-lg-12 mx-auto">
 		<div class="card">
 			<div class="card-body">
-				<div class="card-title text-uppercase "><i class="fa fa-address-book-o"></i>  Bill for {{$bill->enrollee->first_name}} {{$bill->enrollee->middle_name}} {{$bill->enrollee->last_name}} in {{$bill->hcp->hcp_name}} </div>
+				<div class="card-title text-uppercase "><i class="fa fa-address-book-o"></i>  claim for {{$claim->hcp->hcp_name}} at attention to {{$claim->enrollee->first_name}} {{$claim->enrollee->middle_name}} {{$claim->enrollee->last_name}}</div>
 				<hr>
 
-				<section class="content-header">
-					<h3>
-						Invoice
-						<small>#{{$bill->bill->identifier}}</small>
-					</h3>
-				</section>
-
-
-				
-				
-				
-
-				<!-- <div class="form-group row">
-					<div class="col-sm-10">
-						<a href="" type="submit" class="btn btn-primary shadow-primary px-5"><i class="icon-lock"></i> Edit Medical Bill Details</a>
-					</div>
-				</div> -->
 			</div>
 		</div>
 	</div>
